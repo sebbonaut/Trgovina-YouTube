a4de1e5086b99586db401521ba277a24d6c7e9f3
sebas <sebas@DESKTOP-S92RB8H>
2023-10-10 15:46:02 +0200

    Add project files.

---
+++ Trgovina/Artikl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trgovina
+{
+    public enum Kategorija : byte
+    {
+        Pecivo = 2,
+        Napitak,
+        Meso = 5,
+        Ostalo
+    }
+    public class Artikl
+    {
+        public string naziv;
+        decimal cijena, popust;
+        public readonly DateTime stvoren;
+        public static readonly decimal tecaj;
+
+        public Kategorija TipProizvoda { get; init; }
+        public uint Kolicina { get; private set; } = 10;
+
+        public void Kupi(uint komada)
+        {
+            Kolicina -= komada;
+        }
+        
+        public decimal Cijena
+        {
+            get => cijena;
+            //set => cijena = value;
+            set
+            {
+                if(value < 0M)
+                {
+                    throw new ArgumentException("Negativna vrijednost cijene!");
+                }
+                else
+                {
+                    cijena = value;
+                }
+            }
+        }
+        public decimal Popust
+        {
+            set => popust = value;
+        }
+
+        //public decimal Iznos() => cijena * (1 - popust);
+        public decimal Iznos => cijena * (1 - popust);
+        public void IspisHRK() =>
+            Console.WriteLine(Iznos * tecaj + " HRK");
+        static Artikl()
+        {
+            tecaj = 7.5345M;
+        }
+
+        public Artikl(string n, decimal c, decimal p = 0) {
+            TipProizvoda = Kategorija.Ostalo;
+            naziv = n;
+            Cijena = c;
+            Popust = p;
+            stvoren = DateTime.Now;
+        }
+
+        public Artikl(string naziv) : this(naziv, 0)
+        {
+
+        }
+    }
+}
---
+++ Trgovina/Program.cs
@@ -0,0 +1,39 @@
+﻿using Trgovina;
+
+//Console.WriteLine(Artikl.tecaj);
+Artikl a = new ("Med cvjetni 450g", 4.09M);
+Artikl b = new("Kruh")
+{
+    TipProizvoda = Kategorija.Pecivo
+};
+//b.TipProizvoda = Kategorija.Napitak;
+Console.WriteLine(b.TipProizvoda == Kategorija.Pecivo);
+Console.WriteLine((byte)b.TipProizvoda);
+Artikl c = new(c: 1.23M, p: 0.25M, n: "Pahuljice coko");
+//c.Kolicina = 10;
+//c.Kolicina++;
+c.Kupi(2);
+Console.WriteLine($"Kolicina: {c.Kolicina}");
+Console.WriteLine(c.Iznos);
+c.IspisHRK();
+Artikl d = new("Jos jedan namaz")
+{
+    Cijena = 5.32M,
+    Popust = 0.16M,
+    naziv = "Tuna namaz 100g"
+};
+
+try
+{
+    d.Cijena = -5.3M;
+    d.Popust = 0.2M;
+    Console.WriteLine(d.Cijena);
+    //Console.WriteLine(d.Popust);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Kategorija kat = Kategorija.Pecivo;
+Console.WriteLine(kat);
